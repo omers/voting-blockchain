b958a9f64d2a5fd111dedbb0214da6062ca57ea5
Omer Segev <omersegev@gmail.com>
2022-07-03 16:15:37 +0300

    Change .env file

--- bcb_server/peer.py
+++ bcb_server/peer.py
@@ -331,28 +331,3 @@ def join_to_network(orderer, ca, myIP, myPort):
         print("Connection refused by the server..")
         
         return False
-
-""" if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    myIP = get_ip()
-    
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    parser.add_argument('-c', '--ca', default='0.0.0.0', type=str, help='port to listen on')
-    parser.add_argument('-o', '--orderer', default='0.0.0.0', type=str, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-    caIP = args.ca
-    ordererIP = args.orderer
-
-    print('My ip address : ' + get_ip())
-    
-    # time.sleep(5)
-    # join_to_network(ordererIP + ':' + ordererPort, caIP + ':' + caPort, myIP, port)
-    while not join_to_network(ordererIP + ':' + ordererPort, caIP + ':' + caPort, myIP, port):
-        print("Let me sleep for 5 seconds")
-        time.sleep(5)
-
-    app.run(host='0.0.0.0', port=port, debug = True, threaded = True) """
-
