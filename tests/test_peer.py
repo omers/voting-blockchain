1a1efc23d7234280f57f6cc589e05e11c452b34b
Omer Segev <omersegev@gmail.com>
2022-07-04 10:31:42 +0300

    Added Orderer tests

---
+++ tests/test_peer.py
@@ -0,0 +1,30 @@
+from bcb_server.peer import app
+
+def test_new_transaction_endpoint():
+    response = app.test_client().post('/new_transaction')
+    #assert response.status_code == 200
+    pass
+
+def test_get_transaction_endpoint():
+    pass
+
+def test_open_surveys_endpoint():
+    pass
+
+def test_chain_endpoint():
+    pass
+
+def test_local_chain_endpoint():
+    pass
+
+def test_mine():
+    pass
+
+def test_add_block_endpoint():
+    pass
+
+def test_pending_tx_endpoint():
+    pass
+
+def test_list_nodes_endpoint():
+    pass
