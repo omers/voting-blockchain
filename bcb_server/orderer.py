7134b88af4c334f8da8bf798af10cb5a24827b50
Omer Segev <omersegev@gmail.com>
2022-07-04 10:04:17 +0300

    Added IP addr input validation

--- bcb_server/orderer.py
+++ bcb_server/orderer.py
@@ -6,6 +6,7 @@ from bcb_server.utils import get_ip
 
 import json
 import requests
+import ipaddress
 
 app = Flask(__name__)
 
@@ -14,17 +15,21 @@ app = Flask(__name__)
 peers = set()
 
 # endpoint to add new peers to the network.
-@app.route('/add_node', methods=['POST'])
+@app.route("/add_node", methods=["POST"])
 def register_new_peers():
     data = request.get_json()
 
     if not data:
-        return 'Invalid data' , 400
+        return "Invalid data", 400
 
-    request_addr = data['ipaddress']
-    port = data['port']
-    node = request_addr + ':' + str(port)
+    request_addr = data["ipaddress"]
+    try:
+        ipaddress.ip_address(request_addr)
+        port = data["port"]
+    except:
+        return "Invalid Host Address", 400
 
+    node = request_addr + ":" + str(port)
     if not node:
         return "Invalid data", 400
 
@@ -32,7 +37,8 @@ def register_new_peers():
 
     return "Success", 201
 
-@app.route('/broadcast_block', methods=['POST'])
+
+@app.route("/broadcast_block", methods=["POST"])
 def announce_new_block():
     """
     A function to announce to the network once a block has been mined.
@@ -41,7 +47,7 @@ def announce_new_block():
     """
     block = Block.fromDict(request.get_json())
     if not block:
-    	return "Invalid data at announce_new_block", 400
+        return "Invalid data at announce_new_block", 400
 
     request_addr = get_ip(request.remote_addr)
 
@@ -54,7 +60,7 @@ def announce_new_block():
             url = "http://{}/add_block".format(peer)
             requests.post(url, json=block.__dict__)
         except requests.exceptions.ConnectionError:
-            print('Cant connect to node {}. Remove it from peers list'.format(peer))
+            print("Cant connect to node {}. Remove it from peers list".format(peer))
             offline_node.append(peer)
 
     for peer in offline_node:
@@ -62,7 +68,8 @@ def announce_new_block():
 
     return "Success", 201
 
-@app.route('/broadcast_transaction', methods=['POST'])
+
+@app.route("/broadcast_transaction", methods=["POST"])
 def announce_new_transaction():
     """
     A function to announce to the network once a transaction has been added.
@@ -84,7 +91,7 @@ def announce_new_transaction():
             url = "http://{}/get_transaction".format(peer)
             requests.post(url, json=data)
         except requests.exceptions.ConnectionError:
-            print('Cant connect to node {}. Remove it from peers list'.format(peer))
+            print("Cant connect to node {}. Remove it from peers list".format(peer))
             offline_node.append(peer)
 
     for peer in offline_node:
@@ -92,7 +99,8 @@ def announce_new_transaction():
 
     return "Success", 201
 
-@app.route('/consensus', methods=['GET'])
+
+@app.route("/consensus", methods=["GET"])
 def consensus():
     """
     Our simple consensus algorithm. If a longer valid chain is
@@ -100,52 +108,38 @@ def consensus():
     """
     longest_chain = Blockchain()
     current_len = len(longest_chain.chain)
-    
+
     offline_node = []
-    
+
     for peer in peers:
         try:
-            response = requests.get('http://{}/local_chain'.format(peer))
-            length = response.json()['length']
-            chain = response.json()['chain']
+            response = requests.get("http://{}/local_chain".format(peer))
+            length = response.json()["length"]
+            chain = response.json()["chain"]
             new_blockchain = Blockchain.fromList(chain)
 
-            if length > current_len and longest_chain.check_chain_validity(new_blockchain.chain):
+            if length > current_len and longest_chain.check_chain_validity(
+                new_blockchain.chain
+            ):
                 current_len = length
                 longest_chain = new_blockchain
         except requests.exceptions.ConnectionError:
-            print('Cant connect to node {}. Remove it from peers list'.format(peer))
+            print("Cant connect to node {}. Remove it from peers list".format(peer))
             offline_node.append(peer)
 
     for peer in offline_node:
         peers.remove(peer)
-    
+
     chain_data = []
 
     for block in longest_chain.chain:
         chain_data.append(block.__dict__)
 
-    return jsonify({"length": len(chain_data),
-                       "chain": chain_data})
+    return jsonify({"length": len(chain_data), "chain": chain_data})
 
-#get current list of nodes in the network
-@app.route('/list_nodes', methods=['GET','POST'])
+
+# get current list of nodes in the network
+@app.route("/list_nodes", methods=["GET", "POST"])
 def get_node():
-    result = {
-        'Nodes in System' : list(peers),
-        'Count of Nodes' : len(peers)
-    }
+    result = {"Nodes in System": list(peers), "Count of Nodes": len(peers)}
     return jsonify(result)
-
-# if __name__ == '__main__':
-#     from argparse import ArgumentParser
-
-#     parser = ArgumentParser()
-#     parser.add_argument('-p', '--port', default=5002, type=int, help='port to listen on')
-#     args = parser.parse_args()
-#     port = args.port
-
-#     print('My ip address : ' + get_ip())
-
-#     app.run(host='0.0.0.0', port=port, debug = True, threaded = True)
-
